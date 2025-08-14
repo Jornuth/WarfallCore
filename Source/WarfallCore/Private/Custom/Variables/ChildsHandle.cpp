539b6b7e2314923e1f3d49bf0ffea010824330f8
Jornuth <chotin.ytb@gmail.com>
2025-08-14 18:44:21 +0200

    Add files via upload

---
+++ Source/WarfallCore/Private/Custom/Variables/ChildsHandle.cpp
@@ -0,0 +1,30 @@
+﻿#include "Custom/Variables/ChildsHandle.h"
+
+#include "Utils/Tables.h"
+
+// ===============================[ Property Customization Factory ]============================
+// ====================================== FITEMROWHANDLE =======================================
+// =============================================================================================
+
+bool FCustomItemRowHandle::GetFilterConditions(const FName RowName, const FName HandleTag) const
+{
+	const bool bFilterByTag = HandleTag != NAME_None;
+	if (const FItemRowDetail* Row = DataTable->FindRow<FItemRowDetail>(RowName, TEXT("")))
+	{
+		if (!bFilterByTag || Row->HasTag(HandleTag))
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+ETablePath FCustomItemRowHandle::GetDataTable()
+{
+	return ETablePath::ItemsTable;
+}
+
+FString FCustomItemRowHandle::DefaultFilter() const
+{
+	return FCustomBaseHandle::DefaultFilter();
+}
