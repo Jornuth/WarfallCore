08868e942b8a89c2e229f7df6e840bf7613a6359
Jornuth <chotin.ytb@gmail.com>
2025-08-25 18:23:13 +0200

    Delete Source/WarfallCore directory

--- Source/WarfallCore/Public/Inventory/InventoryDebug.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include "Inventory/InventoryTypes.h"
-#include "Inventory/InventoryComponent.h"
-
-namespace WfInvDbg
-{
-	void BuildAsciiGrid(const FContainerData& Data, const FContainerMeta& Meta, TArray<FString>& OutLines);
-	bool UI_ParseContainerId(UInventoryComponent* Inv, const FString& Which, FContainerId& OutId, FContainerData*& OutData);
-}
